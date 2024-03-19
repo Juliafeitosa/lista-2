a801c6a769fc3e518202a3f897b67188ef850be1
juliafeitosa <juliafernanda8705@gmail.com>
2024-03-18 21:13:36 -0300

    exercicio 10

---
+++ ex10.java
@@ -0,0 +1,47 @@
+//10. Crie um programa que leia um vetor A de 5 posições e, ao final da leitura, copieos elementos de A em B de forma invertida.
+// Ou seja, o primeiro elemento de A é oúltimo elemento de B, o segundo elemento de A é o penúltimo elemento de B, e assimpor diante.
+
+
+import java.util.Scanner;
+
+
+public class ex10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        int tamanho = 5;
+
+
+        int[] vetorA = new int[tamanho];
+        int[] vetorB = new int[tamanho];
+
+
+ 
+        System.out.println("Digite os elementos do vetor A:");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("Posição " + i + ": ");
+            vetorA[i] = scanner.nextInt();
+        }
+
+
+        for (int i = 0; i < tamanho; i++) {
+            vetorB[i] = vetorA[tamanho - 1 - i];
+        }
+
+
+        System.out.println("Vetor A: ");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+       
+        System.out.println("Vetor B (cópia de A invertida): ");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print(vetorB[i] + " ");
+        }
+    }
+}
+
