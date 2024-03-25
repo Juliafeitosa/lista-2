f72929eefdc275bb549758f794ec61bf5db1cc94
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:48:28 -0300

    Update ex12.java

--- ex12.java
+++ ex12.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 
 public class Ex12 {
-    public static void positivos(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
