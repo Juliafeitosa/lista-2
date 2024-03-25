96df45999c7f5d62ff0aa3b016127627aa094fdc
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:49:08 -0300

    Update ex15.java

--- ex15.java
+++ ex15.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 
 public class Ex15 {
-    public static void armazenar(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
