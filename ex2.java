f706327014425043e151592285a4000a9be06d1d
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:49:19 -0300

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 
 public class Ex2 {
-    public static void sequencia (String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite o tamanho do vetor: ");
