d7c9759b907b1caf0d6d9f94109ffc0b25947a42
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:52:29 -0300

    Update ex8.java

--- ex8.java
+++ ex8.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 
 public class Ex8 {
-    public static void seguidos (String[] args) {
+    public static void main (String[] args) {
  
         int[] vetorj = {1, 2, 3, 4, 5};
         int[] vetorf = {6, 7, 8, 9, 10};
