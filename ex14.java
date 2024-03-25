42352eef5fead19008206608273b89b071d51102
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:48:55 -0300

    Update ex14.java

--- ex14.java
+++ ex14.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 
 public class Ex14 {
-    public static void maiorElemento(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int tamanho = 5;
 
