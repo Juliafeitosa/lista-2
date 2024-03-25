2c7246f15ea5ae1b202e402d2ed2748d91b69c24
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:49:56 -0300

    Update ex5.java

--- ex5.java
+++ ex5.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 
 public class Ex5 {
-    public static void quantidade (String[] args) {
+    public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
